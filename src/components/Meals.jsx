37cccf3f4a946e3c7bf6eeaf1b0dc27b08c0b19c
Allename <allename.dev@gmail.com>
2023-02-17 23:49:01 +0100

    added more styles

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { device } from "../breakpoint";
 import { useGlobalContext } from "../context"
 
 const Meals = () => {
@@ -9,7 +10,7 @@ const Meals = () => {
       const {idMeal:id, strMeal:title, strMealThumb:image} = singleMeal;
       return (
         <article key={id} className='single-meal'>
-          <img src={image} className="img" style={{width: '200px'}}/>
+          <img src={image} className="img"/>
           <footer>
             <h5>{title}</h5>
             <button className='like-btn'>click me</button>
@@ -26,7 +27,53 @@ const StyledMeals = styled.section`
   max-width: var(--max-width);
   margin: 0 auto;
   display: grid;
+  grid-template-columns: repeat(3, 1fr);
   gap: 2rem;
+
+  .single-meal {
+    background: var(--white);
+    color: var(--textColor);
+    border-radius: var(--borderRadius);
+    box-shadow: var(--shadow-2);
+    transition: var(--transition);
+
+    &:hover {
+      box-shadow: var(--shadow-4);
+    }
+
+    .img {
+      height: 15rem;
+      border-top-left-radius: var(--borderRadius);
+      border-top-right-radius: var(--borderRadius);
+      cursor: pointer;
+    }
+
+    footer {
+      padding: 1rem 1.5rem;
+      display: flex;
+      flex-flow: row wrap;
+      align-items: center;
+      justify-content: space-between;
+
+      h5 {
+        padding: 0;
+        margin: 0;
+      }
+
+      .like-btn {
+        background: transparent;
+        border: transparent;
+        font-size: 1.5rem;
+        cursor: pointer;
+        transition: var(--transition);
+
+        &:hover {
+          color: var(--red-dark);
+          transform: translateY(-2px);
+        }
+      }
+    }
+  }
 `;
 
 export default Meals;
