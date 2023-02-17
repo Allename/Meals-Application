6f78c98f10711d99a79678b24061ba0fc31f38e8
Allename <allename.dev@gmail.com>
2023-02-17 22:19:06 +0100

    added urls

--- src/context.js
+++ src/context.js
@@ -2,6 +2,9 @@ import React, { useContext } from 'react';
 
 const AppContext = React.createContext();
 
+const allNealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
 const AppProvider = ({children}) => {
   return <AppContext.Provider value='hello'>
     {children}
