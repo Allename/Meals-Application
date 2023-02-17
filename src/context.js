076776a17ec4a75fb71748796fc9e738ea3332b8
Allename <allename.dev@gmail.com>
2023-02-17 22:34:05 +0100

    fetched data using axios

--- src/context.js
+++ src/context.js
@@ -1,11 +1,25 @@
-import React, { useContext } from 'react';
+import axios from 'axios';
+import React, { useContext, useEffect } from 'react';
 
 const AppContext = React.createContext();
 
-const allNealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 const AppProvider = ({children}) => {
+  const fetchMeals = async (url) => {
+    try{
+      const {data} = await axios(url)
+      console.log(data.meals)
+    } catch (error) {
+      console.log(error.response);
+    }
+  }
+
+  useEffect(() => {
+    fetchMeals(allMealsUrl)
+  }, [])
+
   return <AppContext.Provider value='hello'>
     {children}
   </AppContext.Provider>
