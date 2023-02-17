18e245705683cb7932c34d9f044a12687d93d41a
Allename <allename.dev@gmail.com>
2023-02-17 21:54:12 +0100

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import {Meals} from "./components";
 import { GlobalStyle } from "./styles/globalStyles";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
+      <Meals/>
     </div>
   );
 }
