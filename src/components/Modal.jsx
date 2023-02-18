8165e58dcd7db833fd84e9d048014e9fe33722a7
Allename <allename.dev@gmail.com>
2023-02-18 04:44:25 +0100

    added styles

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,33 @@
+import styled from "styled-components";
+
 const Modal = () => {
   return <ModalOverlay>
     <div className="modal-container">
-      
+
     </div>
   </ModalOverlay>
 }
+
+const ModalOverlay = styled.aside`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.85);
+  display: grid;
+  place-items: center;
+  transition: var(--transition);
+  z-index: 100;
+
+  .modal-container {
+    width: 80vw;
+    max-width: 800px;
+    height: 80vh;
+    overflow: scroll;
+    background: var(--white);
+    border-radius: var(--borderRadius);
+  }
+`;
+
+export default Modal;
