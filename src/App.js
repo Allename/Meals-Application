1138a2d24cf6c42777ff139a548ec75e603d9f91
Allename <allename.dev@gmail.com>
2023-02-18 03:59:26 +0100

    imported showModal & Modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import {Meals, Search} from "./components";
+import {Meals, Modal, Search} from "./components";
+import { useGlobalContext } from "./context";
 import { GlobalStyle } from "./styles/globalStyles";
 
 function App() {
+  const {showModal} = useGlobalContext;
+
   return (
     <div className="App">
       <GlobalStyle/>
       <Search/>
       <Meals/>
+      {showModal && <Modal/>}
     </div>
   );
 }
