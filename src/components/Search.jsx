d60fc6ac64e4036105cbd28383431dc00291cb02
Allename <allename.dev@gmail.com>
2023-02-18 02:46:38 +0100

    added styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -14,4 +14,37 @@ const Search = () => {
   </StyledSearch>
 }
 
+const StyledSearch = styled.header`
+  height: 5rem;
+  background: var(--white);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    width: var(--view-width);
+    max-width: var(--max-width);
+    display: flex;
+    gap: 0.5rem;
+    flex-flow: row wrap;
+
+    .form-input {
+      max-width: 200px;
+      padding: 0.375rem 0.75rem;
+      border-radius: var(--borderRadius);
+      background: var(--backgroundColor);
+      border: 1px solid var(--gray-200);
+
+      &::placeholder {
+        font-family: inherit;
+        color: var(--grey-400);
+      }
+    }
+
+    .btn {
+      font-size: 0.75rem;
+    }
+  }
+`;
+
 export default Search;
