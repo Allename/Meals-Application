69630da6bb255ac339519108efb9cf6df3fb23ab
Allename <allename.dev@gmail.com>
2023-02-18 03:34:47 +0100

    feat: update

--- src/context.js
+++ src/context.js
@@ -26,11 +26,15 @@ const AppProvider = ({children}) => {
     setLoading(false)
   }
 
+  const fetchRandomMeal = () => {
+    fetchMeals(randomMealUrl);
+  }
+
   useEffect(() => {
     fetchMeals(`${allMealsUrl}${searchTerm}`)
   }, [searchTerm])
 
-  return <AppContext.Provider value={{loading, meals, setSearchTerm}}>
+  return <AppContext.Provider value={{loading, meals, setSearchTerm, fetchRandomMeal}}>
     {children}
   </AppContext.Provider>
 }
